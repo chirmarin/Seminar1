9d8ea15064c4fe1d66209285f1362fe6004cd8f1
chirkova marina <theomarineo@gmail.com>
2022-12-19 01:22:13 +0300

    I added all exs from third seminar 2

--- ex4/Program.cs
+++ ex4/Program.cs
@@ -277,7 +277,7 @@ int[] GetTableOfSquares(int num){
 
 void PrintArray(int[] array){
     for(int i = 0; i < array.Length; i++){
-        Console.WriteLine(array[i]);
+        Console.Write($"{array[i]} ");
     }
 }
 */
