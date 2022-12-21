c543a9df11119273d356c618fe34e16933ed2112
Chirkova Marina <theomarineo@gmail.com>
2022-12-21 17:02:54 +0300

    some exercixes from fourth seminar 2.0

--- ex5/Program.cs
+++ ex5/Program.cs
@@ -14,10 +14,20 @@ for (int rows = 0; rows < 2; rows++) {
 }
 */
 
+/*
 int[,] matrix = new int[3,4];
 PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
 
-void FillArray
+void FillArray(int[,] matr){
+    for (int i = 0; i < matr.GetLength(0); i++) {
+        for (int j = 0; j < matr.GetLength(1); j++){
+            matr[i,j] = new Random().Next(1,10); //[1,10)
+        }            
+    }
+}
 
 void PrintArray(int[,] matr){
     
@@ -28,3 +38,45 @@ void PrintArray(int[,] matr){
         Console.WriteLine();
     }
 }
+*/
+
+//task factorial
+
+//Console.WriteLine(Factorial(3));
+
+/*
+double Factorial (int n){
+    if(n == 1){ // по определению факториала 1! = 1, а также 0! = 1
+        return 1;
+    }
+    else {
+        return n * Factorial(n - 1);
+    }
+}
+
+for (int i = 1; i < 40; i++){
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+*/
+
+//task fibonacci numbers
+
+/*
+f(1) = 1
+f(2) = 1
+f(n) = f(n-1) + f(n-2)
+*/
+/*
+double Fibonacci(int n){
+    if(n == 1 || n == 2){
+        return 1;
+    }
+    else {
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
+
+for (int i = 1; i < 40; i++){
+    Console.WriteLine($"f({i}) => {Fibonacci(i)}");
+}
+*/
