14e7f5d2e3aea08b5b5b3f7ca231421f3f8f7e9d
chirmarin <95381413+chirmarin@users.noreply.github.com>
2022-12-22 01:04:31 +0300

    Update Program.cs

--- ex5/Program.cs
+++ ex5/Program.cs
@@ -1,4 +1,4 @@
-﻿//string [,] table = new string[2,5];
+//string [,] table = new string[2,5];
 
 //String.Empty - default initialization
 // table[0,0]  table[0,1]  table[0,2]  table[0,3]  table[0,4]  
@@ -6,7 +6,6 @@
 
 /*
 table[1,2] = "слово";
-
 for (int rows = 0; rows < 2; rows++) {
     for (int columns = 0; columns < 5; columns++){
         Console.WriteLine($"-{table[rows,columns]}-");
@@ -20,7 +19,6 @@ PrintArray(matrix);
 FillArray(matrix);
 Console.WriteLine();
 PrintArray(matrix);
-
 void FillArray(int[,] matr){
     for (int i = 0; i < matr.GetLength(0); i++) {
         for (int j = 0; j < matr.GetLength(1); j++){
@@ -28,7 +26,6 @@ void FillArray(int[,] matr){
         }            
     }
 }
-
 void PrintArray(int[,] matr){
     
     for (int i = 0; i < matr.GetLength(0); i++) {
@@ -53,7 +50,6 @@ double Factorial (int n){
         return n * Factorial(n - 1);
     }
 }
-
 for (int i = 1; i < 40; i++){
     Console.WriteLine($"{i}! = {Factorial(i)}");
 }
@@ -75,8 +71,47 @@ double Fibonacci(int n){
         return Fibonacci(n - 1) + Fibonacci(n - 2);
     }
 }
-
 for (int i = 1; i < 40; i++){
     Console.WriteLine($"f({i}) => {Fibonacci(i)}");
 }
 */
+
+/*Задача 24. Напишите программу, которая принимает на вход число (А) и выдает сумму чисел от 1 до А
+7 -> 28   4-> 10   8 -> 36*/
+
+/*
+bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+
+if(!isParsed || number < 1){
+    Console.WriteLine("Неверное число!");
+    return;
+}
+
+int sum = GetSum(number);
+Console.WriteLine(sum);*/
+
+/*
+
+int[] numbers = new int[]{1,2,3,4};
+int[] sums = new int[]{1,3,6,10};
+
+for (var i = 0; i < numbers.Length; i++){
+    int sum = GetSum(numbers[i]);
+    if (sum != sums[i]){
+        Console.WriteLine($"Ожидали получить {sums[i]}, получили -> {sum}");
+    }
+    else{
+        Console.WriteLine("Все ок!");
+    }
+}
+
+int GetSum(int number){
+    int sum = 0;
+    for(var i = 1; i <= number; i++){
+        sum = sum + i;
+    }
+    return sum;
+}
+
+*/
+
