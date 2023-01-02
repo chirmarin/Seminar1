50e9d37d82c7e883dd1e0323bee13d3214a766ab
chirkova marina <theomarineo@gmail.com>
2023-01-02 15:35:44 +0300

    add exs from 5 seminar

---
+++ ex7/Program.cs
@@ -0,0 +1,237 @@
+﻿/*5 seminar*/
+
+/*
+    Задача 31. Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]
+    Найдите сумму отрицательных и положительных элементов массива. Например, в массиве:
+    [3, 9, -8, 1, 0, -7, 2, -1, 8, -3, -1, 6]
+    сумма положительных чисел равна 29, сумма отрицательных равна -20
+*/
+
+/*
+int[] array = GenerateArray(-9, 9, 12);
+PrintArray(array);
+int sumPositiveNumbers = GetSumPositiveNumbersInArray(array);
+int sumNegativeNumbers = GetSumNegativeNumbersInArray(array);
+Console.WriteLine($"Сумма положительных = {sumPositiveNumbers}, сумма отрицательных = {sumNegativeNumbers}");
+
+//Methods
+int[] GenerateArray(int min, int max, int length){
+    Random random = new Random();
+    int[] array = new int[length];
+
+    for(var i = 0; i < array.Length; i++){
+        array[i] = random.Next(min, max + 1);
+    }
+    return array;
+}
+
+int GetSumPositiveNumbersInArray(int[] array){
+    int sumPositiveNumbers = 0;
+    for(var i = 0; i < array.Length; i++){
+        if(array[i] >= 0){
+            sumPositiveNumbers+= array[i];
+        }
+    }
+    return sumPositiveNumbers;
+}
+
+int GetSumNegativeNumbersInArray(int[] array){
+    int sumNegativeNumbers = 0;
+    for(var i = 0; i < array.Length; i++){
+        if(array[i] < 0){
+            sumNegativeNumbers+= array[i];
+        }
+    }
+    return sumNegativeNumbers;
+}
+
+void PrintArray(int[] array){
+    //Console.WriteLine(@"[{0}]", string.Join(", ", array));
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+*/
+
+/*
+    Напишите программу, которая выполняет замену элементов массива:
+    положительные элементы замените на соответствующие отрицательные и наоборот.
+    [-4, -8, 8, 2] -> [4, 8, -8, -2]
+*/
+/*
+int[] initArray = GenerateArray(-10, 10, 5);
+PrintArray(initArray);
+int[] outArray = PosNegReplace(initArray);
+PrintArray(outArray);
+
+//Methods
+int[] GenerateArray(int min, int max, int length){
+    Random random = new Random();
+    int[] array = new int[length];
+
+    for(var i = 0; i < array.Length; i++){
+        array[i] = random.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array){
+    //Console.WriteLine(@"[{0}]", string.Join(", ", array));
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+
+int[] PosNegReplace(int[] array){
+    int[] arr = new int[array.Length]; // не трогаем исходный массив
+    for(int i = 0; i < array.Length; i++){
+        arr[i] = -array[i];
+    }
+    return arr;
+}
+*/
+
+/*
+    Задайте массив. Напишите программу, которая определяет присутствует ли
+    заданное число в массиве.
+    4; массив [6, 7, 19, 345, 3] -> нет
+    3; массив [6, 7, 19, 345, 3] -> да
+
+    int[] a = new int[];
+    int[] a = new int[4]{1,2,34,5};
+    a.Contains(12);//bool
+*/
+/*
+Console.WriteLine("Введите целое число: ");
+bool isInteger = int.TryParse(Console.ReadLine(), out int number);
+if(!isInteger){
+    Console.WriteLine("Неверный ввод!");
+    return;    
+}
+
+int[] initArray = GenerateArray(-100, 100, 100);
+PrintArray(initArray);
+if(isExist(initArray, number)){
+    Console.WriteLine("Число есть в массиве");
+}
+else{
+    Console.WriteLine("Числа нет в массиве");
+}
+
+//Methods
+int[] GenerateArray(int min, int max, int length){
+    Random random = new Random();
+    int[] array = new int[length];
+
+    for(var i = 0; i < array.Length; i++){
+        array[i] = random.Next(min, max + 1);
+    }
+    return array;
+}
+
+bool isExist(int[] array, int numToFind){
+    bool result = false;
+    for(var i = 0; i < array.Length; i++){
+        if(array[i] == numToFind){
+            result = true;
+            break; 
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[] array){
+    //Console.WriteLine(@"[{0}]", string.Join(", ", array));
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+*/
+
+/*
+    Задайте одномерный массив из 123 случайных чисел.
+    Найдите количество элементов массива, значения которых лежат в отрезке [10, 99]
+
+    Пример для массива из 5, а не из 123 элементов. 
+    В своем решении сделайте для 123
+    [5, 18, 123, 6, 2] -> 1
+    [1, 2, 3, 6, 2] -> 0
+    [10, 11, 12, 13, 14 -> 5
+*/
+/*
+int[] initArray = GenerateArray(-100, 100, 10);
+PrintArray(initArray);
+int min = 10;
+int max = 99;
+Console.WriteLine($"Количество элементов на отрезке от {min} до {max} = {findElementsinSegment(initArray, min, max)}");
+
+//Methods
+int[] GenerateArray(int min, int max, int length){
+    Random random = new Random();
+    int[] array = new int[length];
+
+    for(var i = 0; i < array.Length; i++){
+        array[i] = random.Next(min, max + 1);
+    }
+    return array;
+}
+
+int findElementsinSegment(int[] array, int min, int max){
+    int count = 0;
+    for(var i = 0; i < array.Length; i++){
+        if(array[i] >= min && array[i] <= max){
+            count++;
+        }
+    }
+    return count;
+}
+
+void PrintArray(int[] array){
+    //Console.WriteLine(@"[{0}]", string.Join(", ", array));
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+*/
+
+/*
+    Найдите произведение пар чисел в одномерном массиве.
+    Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+    Результат запишите в новом массиве.
+    [1 2 3 4 5] -> 5 8 3
+    [6 7 3 6] -> 36 21
+*/
+/*
+int[] initArray = GenerateArray(-10, 10, 10);
+PrintArray(initArray);
+int[] prodArray = SymmElementsProducts(initArray);
+PrintArray(prodArray);
+
+//Methods
+int[] GenerateArray(int min, int max, int length){
+    Random random = new Random();
+    int[] array = new int[length];
+
+    for(int i = 0; i < array.Length; i++){
+        array[i] = random.Next(min, max + 1);
+    }
+    return array;
+}
+
+int SymmElementsProducts(int[] arr){
+    int[] arr2 = new int[arr.Length/2 + arr.Length%2];
+    for(int i = 0; i < arr2.Length; i++){
+        if(i != arr.Length - i - 1){
+            arr2[i] = arr[i] * arr[arr.Length - i -1];            
+        }
+        else{
+            arr2[i] = arr[i];
+        }
+    }
+    return arr2;
+}
+
+void PrintArray(int[] array){
+    //Console.WriteLine(@"[{0}]", string.Join(", ", array));
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+*/
+
+
+/*6 seminar*/
+
+
+
+/*7 seminar*/
